61a03ec660d1524718f9dcca9629c16a0f71b3c8
akella <akella.a@gmail.com>
2015-04-24 00:57:36 +0300

    animations

--- js/common.js
+++ js/common.js
@@ -1,19 +1,8 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	$('body').click(function(event) {
+		$(this).toggleClass('figure1');
+	});
 
 	console.log($('body').html());
 });
