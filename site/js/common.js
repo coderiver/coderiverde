c0c502d206f4fe646518827816547291f180bcd3
akella <akella.a@gmail.com>
2015-04-28 14:48:50 +0300

    animations

--- site/js/common.js
+++ site/js/common.js
@@ -1,8 +1,16 @@
 head.ready(function() {
-	$('body').click(function(event) {
-		$(this).toggleClass('figure1');
-	});
-	
+	// $('body').click(function(event) {
+	// 	$(this).toggleClass('figure1');
+	// });
+
+
+	var i = 0; 
+	function changeClass(){
+	    $("body").removeClass("figure" + i)
+	        i = (i==3)?1:i+1;    
+	    $("body").addClass("figure" + i);
+	}
+	setInterval(changeClass, 2000);
     console.log('Hello from the Hell');
 
 });
